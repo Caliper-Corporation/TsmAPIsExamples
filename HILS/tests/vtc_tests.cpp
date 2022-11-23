b3b378b537df014b067290140137a74ca40fe337
wxinix-caliper <wuping@caliper.com>
2022-11-22 20:01:04 -0500

    Update HILS license.

--- HILS/tests/vtc_tests.cpp
+++ HILS/tests/vtc_tests.cpp
@@ -1,22 +1,40 @@
-/*
+/*!
+BSD 3 - Clause License
+
+Copyright(c) 2022, Caliper Corporation
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and /or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*!
 C++ Virtualization Library of Traffic Control Cabinet
 Copyright (C) 2022  Wuping Xin
 
-GNU GPL v3 License
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or at
-your option any later version.
-
-This program is distributed in the hope that it will be useful,  but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
-ABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License with
-this program. If not, see <https://www.gnu.org/licenses/>.
+MPL 1.1/GPL 2.0/LGPL 2.1 tri-license
 */
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
